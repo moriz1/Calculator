169fa8509d097986a859366664b484c8565921c5
moriz1 <tehmoriz@gmail.com>
2015-09-14 20:38:52 -0400

    Initial logic implementation

---
+++ Assets/CalculatorLogic.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CalculatorLogic : MonoBehaviour {
+
+	public Text InputBox;
+	public Text HistoryBox;
+
+	public void On0ButtonClicked() {
+	}
+	public void On1ButtonClicked() {
+	}
+	public void On2ButtonClicked() {
+	}
+	public void On3ButtonClicked() {
+	}
+	public void On4ButtonClicked() {
+	}
+	public void On5ButtonClicked() {
+	}
+	public void On6ButtonClicked() {
+	}
+	public void On7ButtonClicked() {
+	}
+	public void On8ButtonClicked() {
+	}
+	public void On9ButtonClicked() {
+	}
+	public void OnEqualButtonClicked() {
+	}
+}
