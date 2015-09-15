f5de4cc6b567737d9a5a74162b4318156b5af63a
moriz1 <tehmoriz@gmail.com>
2015-09-15 03:27:17 -0400

    Initial calculation logic
    Calculator now functions… sort of. Editor crashes if I try to enter
    4+4*4, or anything of similar format.

--- Assets/CalculatorLogic.cs
+++ Assets/CalculatorLogic.cs
@@ -14,8 +14,9 @@ public class CalculatorLogic : MonoBehaviour {
 	public Text InputBox;
 	public Text HistoryBox;
 
-	private Stack<string> ParserStack;
+	private Stack<string> SymbolStack;
 	private Queue<string> InputQueue;
+	private Queue<string> OutputQueue;
 
 	private string currentInputToken;
 
@@ -34,14 +35,31 @@ public class CalculatorLogic : MonoBehaviour {
 			Debug.Log("HistoryBox object not assigned!");
 		}
 
-		ParserStack = new Stack<string> ();
+		SymbolStack = new Stack<string> ();
 		InputQueue = new Queue<string> ();
+		OutputQueue = new Queue<string> ();
+
 		currentInputToken = DefaultInputBoxText;
 	}
 
 	public void OnButtonClicked(Text t) {
 		if (t.text == "EQUAL") {
+			if (InputQueue.Count > 0) {
+				InputQueue.Enqueue(currentInputToken);
 
+				ToRPN();
+
+				double answer = Calculate();
+				HistoryBox.text = HistoryBox.text + InputBox.text + "\n" + "= " + answer + "\n";
+				InputBox.text = string.Empty + answer;
+
+				InputQueue.Clear();
+				OutputQueue.Clear();
+				SymbolStack.Clear();
+
+				currentInputToken = answer.ToString();
+				InputQueue.Enqueue(currentInputToken);
+			}
 		}
 		else {
 			switch (t.text) {
@@ -98,24 +116,131 @@ public class CalculatorLogic : MonoBehaviour {
 		}
 	}
 
-	private void Calculate() {
+	private void ToRPN() {
 		string token;
+		double number;
 
 		while (InputQueue.Count > 0) {
 			token = InputQueue.Dequeue();
 
-			switch (token) {
+			if (double.TryParse(token, out number)) {
+				OutputQueue.Enqueue(token);
+			}
 
-			case "+":
-			case "-":
-			case "*":
-			case "/":
-			case "%":
-				break;
-			default:
-				ParserStack.Push(token);
-				break;
+			if (IsOperator(token)) {
+				while ((SymbolStack.Count > 0) && IsOperator(SymbolStack.Peek())) {
+					//since all supported operators are left associative, i'm going to skip this check
+					if (ComparePrecedence(token, SymbolStack.Peek()) <= 0) {
+						OutputQueue.Enqueue(SymbolStack.Pop());
+						break;
+					}
+				}
+
+				SymbolStack.Push(token);
+			}
+
+			if (token == "(") {
+				SymbolStack.Push(token);
+			}
+
+			if (token == ")") {
+				while ((SymbolStack.Count > 0) && (SymbolStack.Peek() != "(")) {
+					OutputQueue.Enqueue(SymbolStack.Pop());
+				}
+
+				SymbolStack.Pop();
 			}
 		}
+
+		while (SymbolStack.Count > 0) {
+			OutputQueue.Enqueue(SymbolStack.Pop());
+		}
+	}
+
+	private double Calculate() {
+		Stack<string> calcStack = new Stack<string> ();
+		string token;
+
+		while (OutputQueue.Count > 0) {
+			token = OutputQueue.Dequeue();
+
+			if (!IsOperator(token)) {
+				calcStack.Push(token);
+			}
+			else {
+				//pop top two tokens
+				double d2 = ConvertToDouble(calcStack.Pop());
+				double d1 = ConvertToDouble(calcStack.Pop());
+
+				double result = token.CompareTo("+") == 0 ? d1 + d2 :
+								token.CompareTo("-") == 0 ? d1 - d2 :
+								token.CompareTo("*") == 0 ? d1 * d2 :
+								token.CompareTo("/") == 0 ? d1 / d2 :
+															d1 % d2;
+
+				calcStack.Push(result.ToString());
+			}
+		}
+
+		return ConvertToDouble(calcStack.Pop());
+	}
+
+	private bool IsOperator(string token) {
+		switch (token) {
+			
+		case "+":
+		case "-":
+		case "*":
+		case "/":
+		case "%":
+			return true;
+		default:
+			return false;
+		}
+	}
+
+	private bool IsLessThanOrEqualPrecedence() {
+		if ((SymbolStack.Peek () == "*") || (SymbolStack.Peek () == "/")
+			|| (SymbolStack.Peek () == "%")) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	private int ComparePrecedence(string token, string top) {
+		int left = 0;
+		int right = 0;
+
+		if (token == "*" || token == "/" || token == "%") {
+			left = 0;
+		}
+		else {
+			left = 1;
+		}
+
+		if (top == "*" || top == "/" || top == "%") {
+			right = 0;
+		}
+		else {
+			right = 1;
+		}
+
+		return right - left;
+	}
+
+	private double ConvertToDouble(string token) {
+		double result;
+
+		try {
+			result = Convert.ToDouble(token);
+		}
+		catch (FormatException) {
+			Debug.Log("Incorrect token type passed to ConvertToDouble!");
+			result = 0;
+		}
+
+		return result;
 	}
 }
